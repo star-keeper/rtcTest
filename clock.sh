de378324f10e4ccb159dbf55dcf9b42bd6103d7d
Alissa Tinney <atinney@wellesley.edu>
2019-12-05 18:19:27 -0500

    working version

--- clock.sh
+++ clock.sh
@@ -3,14 +3,14 @@
 currentHour=1 #h
 secInHour=60 #s
 interval=5 #s
-boot=90 #s documentation states ntp updates every minute
+boot=0 #s documentation states ntp updates every minute
 
 cd /home/pi/Documents/rtcTest
 
 #create a data folder
 counts=$(ls -l | wc -l)
-counts=$((counts-7)) #only count data folders
-mkdir data$counts
+counts=$((counts-9)) #only count data folders
+mkdir data${counts}
 
 #ensure ntp is enabled to start
 sudo systemctl enable ntp
@@ -26,7 +26,7 @@ sudo hwclock -w
 #note: might need to switch to hwclock commands
 #note: do I need to make sure the rtc is set up here? or wait for it?
 while true; do
-	exec 3<> data/${currentHour}currentHour.csv
+	exec 3<> data${counts}/${currentHour}currentHour.csv
 	timedatectl show --property=TimeUSec --value >&3 #first reliable time
 	sudo systemctl stop ntp
 	sudo systemctl disable ntp
