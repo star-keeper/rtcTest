42fe60bece77f9ef01c2f11f8810140f77adb9f0
Alissa Tinney <40249360+star-keeper@users.noreply.github.com>
2019-12-05 18:50:27 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,13 @@ while true; do
 	read -p "Have you enabled I2C? [yn] > " yn
 	case $yn in
 		[Yy]* ) echo "Good, let's continue"; break;;
-		[Nn]* ) echo "You will be redirected to configure I2C in 10 seconds."; echo "   5 Interfacing Options > P5 I2C > Yes > Ok > Finish"; sleep 10; sudo raspi-config; break;;
+		[Nn]* ) echo "You will be redirected to configure I2C in 10 seconds."; break;;
 		* ) echo "Please answer [y] or [n].";;
 	esac
 done
+
+echo "   5 Interfacing Options > P5 I2C > Yes > Ok > Finish"
+lxterminal -e sudo raspi-config
 
 while true; do
 	read -p "Are you connected to the internet? [yn] > " yn
